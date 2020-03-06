c68210519d56abec369d124b351398c94579524f
啊亮 <2491016871@qq.com>
2020-03-06 17:52:00 +0800

    chat

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tony/projects/chat/chatserver/src/server/chatserver.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/chatservice.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/db/db.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/db/dbpool.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/db/dbpool.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/main.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/model/friendmoel.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/friendmoel.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/model/groupmodel.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/model/offlinemessagemodel.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/model/usermodel.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/tony/projects/chat/chatserver/src/server/redis/redis.cpp" "/home/tony/projects/chat/chatserver/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/server/db/db.cpp
@@ -0,0 +1,89 @@
+#include "db.h"
+#include <muduo/base/Logging.h>
+
+/*
+// 数据库配置信息
+static string server = "127.0.0.1";
+static string user = "root";
+static string password = "123456";
+static string dbname = "chat";
+*/
+
+// 初始化数据库连接
+MySQL::MySQL()
+{
+    _conn = mysql_init(nullptr);
+}
+
+// 释放数据库连接资源
+MySQL::~MySQL()
+{
+    if (_conn != nullptr)
+        mysql_close(_conn);
+}
+
+// 连接数据库
+bool MySQL::connect(string ip, unsigned short port,
+                    string username, string password, string dbname)
+{
+    /*
+    MYSQL *p = mysql_real_connect(_conn, server.c_str(), user.c_str(),
+                                  password.c_str(), dbname.c_str(), 3306, nullptr, 0);*/
+    MYSQL *p = mysql_real_connect(_conn, ip.c_str(), username.c_str(),
+                                  password.c_str(), dbname.c_str(), port, nullptr, 0);
+    if (p != nullptr)
+    {
+        // C和C++代码默认的编码字符是ASCII，如果不设置，从MySQL上拉下来的中文显示？
+        mysql_query(_conn, "set names gbk");
+        LOG_INFO << "connect mysql success!";
+    }
+    else
+    {
+        LOG_INFO << "connect mysql fail!";
+    }
+
+    return p != nullptr;
+}
+
+// 更新操作
+bool MySQL::update(string sql)
+{
+    if (mysql_query(_conn, sql.c_str()))
+    {
+        LOG_INFO << __FILE__ << ":" << __LINE__ << ":"
+                 << sql << "更新失败!";
+        return false;
+    }
+
+    return true;
+}
+
+// 查询操作
+MYSQL_RES *MySQL::query(string sql)
+{
+    if (mysql_query(_conn, sql.c_str()))
+    {
+        LOG_INFO << __FILE__ << ":" << __LINE__ << ":"
+                 << sql << "查询失败!";
+        return nullptr;
+    }
+
+    return mysql_use_result(_conn);
+}
+
+// 获取连接
+MYSQL *MySQL::getConnection()
+{
+    return _conn;
+}
+
+// 刷新一下连接的起始的空闲时间点
+void MySQL::refreshAliveTime()
+{
+    _alivetime = clock();
+}
+// 返回存活的时间
+clock_t MySQL::getAliveTime() const
+{
+    return clock() - _alivetime;
+}
